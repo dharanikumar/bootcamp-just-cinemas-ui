4cf4879bdb9217d80e8495836b403dcdfe80372e
dharanikumar <dharani939@gmail.com>
2019-02-18 17:48:05 +0530

    added test for Header & footer  - Kunal-Dharani

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Header';
+import Footer from './Footer';
 
-it('renders without crashing', () => {
+it('renders App without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders Header without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders Footer without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Footer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
